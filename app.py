a032d22e4f7c825fbd9b89072730f0d296690813
Priyanshu kumar <priyanshu12656@gmail.com>
2024-12-04 13:42:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -106,6 +106,6 @@ def top_drugs_extractor(condition,df):
 from app import app
 if __name__ == "__main__":
 
-    app.run(debug=True, host="0,0,0,0", port=8080)
+    app.run(debug=True, host="localhost", port=8080)
 
 # python -m run flask
